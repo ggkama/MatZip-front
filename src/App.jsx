be04231c6f08f844f94d58df331e1db67158b4c8
parkjin-sol <83712091+parkjin-sol@users.noreply.github.com>
2025-06-20 12:23:35 +0900

    frontend_모듈설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Test from "./pages/Home/Test";
+import Header from "./components/Common/Header/Header";
+import Footer from "./components/Common/Footer/Footer";
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Test />} />
-      </Routes>
-    </AuthProvider>
+    <>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
