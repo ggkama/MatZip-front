95ba79202b10c6aca333cf5da09e71a1c3b19001
ggkama <ggkama@naver.com>
2025-06-20 11:24:37 +0900

    front module set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
-import { AuthProvider } from "./Context/AuthContext";
 import { Routes, Route } from "react-router-dom";
 import Test from "./pages/Home/Test";
 
 function App() {
   return (
-    <AuthProvider>
       <Routes>
         <Route path="/" element={<Test />} />
       </Routes>
-    </AuthProvider>
   );
 }
 
