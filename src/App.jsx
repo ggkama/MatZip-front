b020280159a61e4b7eaf1b784fde7b0838582b52
[2Seongmin] <tjdrmffktm0928@gmail.com>
2025-06-27 16:40:51 +0900

    feature/login_signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Test from "./pages/Home/Test";
 import Header from "./components/Common/Header/Header";
 import Footer from "./components/Common/Footer/Footer";
 import Mypage from "./components/Mypage/Mypage";
+import SignUp from "./components/Member/SignUp";
+import Login from "./components/Member/Login";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/test" element={<Test />} />
           <Route path="/my-page" element={<Mypage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
       <Footer />
